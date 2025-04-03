40964c9eec5be4d5d5ebb0341e9a8f9fafabfbaa
JoseOttati <150614319+JoseOttati@users.noreply.github.com>
2025-04-03 09:38:35 -0500

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,7 +1,7 @@
 
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Jose Ottati/ SECTION 002
  *
  *   Note, additional comments provided throughout this source code
  *   is for educational purposes
